016375cbbf26d5be84321529d9a3803764175207
Martin Lloyd L. Duran <lloyduran@gmail.com>
2018-06-05 22:59:12 +0800

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "standard",
+
+    "rules": {
+        "eqeqeq": ["error", "always"],
+        "quotes": ["warn", "single", {"avoidEscape":false, "allowTemplateLiterals":true}],
+    }
+};
